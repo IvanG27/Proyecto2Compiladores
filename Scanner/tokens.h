f46502765b1cfd0371ec7773c94ca43369c8c5f0
AzulMtz <55148032+AzulMtz@users.noreply.github.com>
2021-12-07 14:43:31 -0600

    Analizador léxico

---
+++ Scanner/tokens.h
@@ -0,0 +1,42 @@
+#ifndef TOKENS_H
+#define TOKENS_H
+
+        const int INT = 1;
+        const int FLOAT = 2;
+        const int ELSE = 3;
+        const int IF = 4;
+        const int WHILE = 5;
+        const int MAS = 6;
+        const int MENOS = 7;
+        const int MUL = 8;
+        const int DIV = 9;
+        const int ASIG = 10;
+        const int LPAR = 11;
+        const int RPAR = 12;
+        const int ID = 13;
+        const int PYC = 14;
+        const int COMA = 15;
+        const int NUMERO = 16;
+        const int BLANCO = 17;
+        const int LKEY = 18;
+        const int RKEY = 19;
+        const int CHAR = 20;
+        const int DOUBLE = 21;
+        const int STRUCT = 22;
+        const int VOID = 23; 
+        const int DO = 24;
+        const int PRINT = 25;
+        const int CADENA = 26;
+        const int PUNTO = 27;
+        const int SCAN = 28;
+        const int BREAK = 29;
+        const int RETURN = 30;
+        const int OR = 31;
+        const int AND = 32;
+        const int MAYOR = 33;
+        const int MENOR = 34;
+        const int DIF = 35;
+        const int CARACTER = 36;
+
+
+#endif
